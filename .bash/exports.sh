bc403ab4ccb8104e4bd10ad031704e4c354a47d2
Tyler Schwemley <tjschwem@gmail.com>
2020-08-07 01:20:21 +0000

    update fzf default to show hidden and ignore .git

--- .bash/exports.sh
+++ .bash/exports.sh
@@ -1,7 +1,8 @@
 # FZF
+
 # Set ag as the default source for fzf
-#export FZF_DEFAULT_COMMAND='ag -p ~/.gitignore -g ""'
-export FZF_DEFAULT_COMMAND='ag -p ./.gitignore -g ""'
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore-dir .git -p ./.gitignore -g ""'
+
 # Apply the command to ctrl-t as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
