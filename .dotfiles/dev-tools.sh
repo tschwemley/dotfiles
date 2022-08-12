2418d27d75b5fef1d138540c5357ebc79193e45f
Tyler Schwemley <tschwemley@zynga.com>
2022-08-11 23:58:25 -0400

    adding gobang and config

---
+++ .dotfiles/dev-tools.sh
@@ -0,0 +1,5 @@
+#install rust
+curl https://sh.rustup.rs -sSf | sh
+
+# install gobang (db tui)
+cargo install --version 0.1.0-alpha.5 gobang
---
+++ .dotfiles/mac.sh
@@ -0,0 +1,1 @@
+export PATH="$PATH:/Users/tschwemley/Library/Python/3.8/bin"
