44b74bec0bbad301eb67360efbdb26c051b29b63
Tyler Schwemley <tschwemley@zynga.com>
2022-09-01 22:55:55 -0400

    updated dev-env

--- .dotfiles/dev-env.sh
+++ .dotfiles/dev-env.sh
@@ -1,11 +1,11 @@
 BIN_PATH=$HOME/bin
 
 # Install zsh if it doesn't exist
-# command zsh
-# if [ $? -gt 0 ]
-# then
-#   sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)"
-# fi
+command zsh -v
+if [ $? -gt 0 ]
+then
+  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)"
+fi
 
 # TODO: add addtional OS support
 # if [[ $OSTYPE == "darwin"* ]]; then 
@@ -14,12 +14,14 @@ BIN_PATH=$HOME/bin
 #   
 #   # Install htop
 #   brew install htop
+#   brew install httpie
 # fi
 
+# TODO switch to navi
 # install cheat.sh (cheat sheet)
-if [ ! -f $BIN_PATH/cheat ]; then
-  curl https://cht.sh/:cht.sh > "$BIN_PATH/cheat"
-fi
+# if [ ! -f $BIN_PATH/cheat ]; then
+#   curl https://cht.sh/:cht.sh > "$BIN_PATH/cheat"
+# fi
 
 # install lazygit
 go install github.com/jesseduffield/lazygit@latest
