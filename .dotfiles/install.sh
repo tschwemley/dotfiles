f7634ab7198281b07aa9630d7d69ce42ea899375
Tyler Schwemely <tjschwem@gmail.com>
2022-09-11 18:46:04 +0000

    move install file location

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,1 +1,10 @@
-source ./dev-tools.sh
+#!/bin/bash
+
+# This script is meant to be ran via curl piping
+git clone --bare https://github.com/tschwemley/dotfiles $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config checkout
+config config status.showUntrackedFiles no
+
+# Bootstrap the dotfiles
+./bootstrap.sh
