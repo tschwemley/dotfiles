6a491182b1112a7863f9b9acbdd4c9d5956f5f60
Tyler Schwemley <me@tylerschwemley.com>
2022-09-21 18:56:48 -0400

    update lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -69,6 +69,8 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.dap.active = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
+lvim.builtin.lir.active = false
+lvim.builtin.nvimtree.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
@@ -92,6 +94,7 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.rainbow.enable = true
 
 -- generic LSP settings
 
@@ -173,25 +176,204 @@ lvim.plugins = {
   -- Aesthetics
   { "ellisonleao/glow.nvim" }, -- Markdown renderer
 
+  -- Databases
+  { "tpope/vim-dadbod" },
+  { "kristijanhusak/vim-dadbod-ui" },
+
   -- Themes
   { "sainnhe/everforest" },
   { "sainnhe/gruvbox-material" },
 
-  -- Databases
-  { "tpope/vim-dadbod" },
-  { "kristijanhusak/vim-dadbod-ui" },
+  -- Utility
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- Trialing
+  {
+    "nvim-neorg/neorg",
+    config = function()
+      require('neorg').setup {
+        -- ... -- check out setup part...
+        load = {
+          ["core.defaults"] = {},
+          ["core.norg.dirman"] = {
+            config = {
+              workspaces = {
+                personal_wiki = "~/logseq",
+                -- home = "~/notes/home",
+              }
+            }
+          }
+        }
+      }
+    end,
+    requires = "nvim-lua/plenary.nvim"
+  },
+  {
+    "renerocksai/telekasten.nvim",
+    config = function()
+      local home = "/home/tschwemley/logseq"
+      require('telekasten').setup({
+        home = home,
+
+        -- if true, telekasten will be enabled when opening a note within the configured home
+        take_over_my_home = true,
+
+        -- auto-set telekasten filetype: if false, the telekasten filetype will not be used
+        --                               and thus the telekasten syntax will not be loaded either
+        auto_set_filetype = true,
+
+        -- dir names for special notes (absolute path or subdir name)
+        dailies = home .. '/' .. 'journals',
+        weeklies = nil,
+        templates = nil,
+        -- weeklies  = home .. '/' .. 'weekly',
+        -- templates = home .. '/' .. 'templates',
+
+        -- image (sub)dir for pasting
+        -- dir name (absolute path or subdir name)
+        -- or nil if pasted images shouldn't go into a special subdir
+        image_subdir = "img",
+
+        -- markdown file extension
+        extension = ".md",
+
+        -- Generate note filenames. One of:
+        -- "title" (default) - Use title if supplied, uuid otherwise
+        -- "uuid" - Use uuid
+        -- "uuid-title" - Prefix title by uuid
+        -- "title-uuid" - Suffix title with uuid
+        new_note_filename = "title",
+        -- file uuid type ("rand" or input for os.date()")
+        uuid_type = "%Y%m%d%H%M",
+        -- UUID separator
+        uuid_sep = "-",
+
+        -- following a link to a non-existing note will create it
+        follow_creates_nonexisting = true,
+        dailies_create_nonexisting = true,
+        weeklies_create_nonexisting = false,
+
+        -- skip telescope prompt for goto_today and goto_thisweek
+        journal_auto_open = false,
+
+        -- template for new notes (new_note, follow_link)
+        -- set to `nil` or do not specify if you do not want a template
+        -- template_new_note = home .. '/' .. 'templates/new_note.md',
+        template_new_note = nil,
+
+        -- template for newly created daily notes (goto_today)
+        -- set to `nil` or do not specify if you do not want a template
+        -- template_new_daily = home .. '/' .. 'templates/daily.md',
+        template_new_daily = nil,
+
+        -- template for newly created weekly notes (goto_thisweek)
+        -- set to `nil` or do not specify if you do not want a template
+        template_new_weekly = nil,
+
+        -- image link style
+        -- wiki:     ![[image name]]
+        -- markdown: ![](image_subdir/xxxxx.png)
+        image_link_style = "markdown",
+
+        -- default sort option: 'filename', 'modified'
+        sort = "filename",
+
+        -- integrate with calendar-vim
+        plug_into_calendar = false,
+        calendar_opts = {
+          -- calendar week display mode: 1 .. 'WK01', 2 .. 'WK 1', 3 .. 'KW01', 4 .. 'KW 1', 5 .. '1'
+          weeknm = 4,
+          -- use monday as first day of week: 1 .. true, 0 .. false
+          calendar_monday = 1,
+          -- calendar mark: where to put mark for marked days: 'left', 'right', 'left-fit'
+          calendar_mark = 'left-fit',
+        },
+
+        -- telescope actions behavior
+        close_after_yanking = false,
+        insert_after_inserting = true,
+
+        -- tag notation: '#tag', ':tag:', 'yaml-bare'
+        tag_notation = "#tag",
+
+        -- command palette theme: dropdown (window) or ivy (bottom panel)
+        command_palette_theme = "ivy",
+
+        -- tag list theme:
+        -- get_cursor: small tag list at cursor; ivy and dropdown like above
+        show_tags_theme = "ivy",
+
+        -- when linking to a note in subdir/, create a [[subdir/title]] link
+        -- instead of a [[title only]] link
+        subdirs_in_links = true,
+
+        -- template_handling
+        -- What to do when creating a new note via `new_note()` or `follow_link()`
+        -- to a non-existing note
+        -- - prefer_new_note: use `new_note` template
+        -- - smart: if day or week is detected in title, use daily / weekly templates (default)
+        -- - always_ask: always ask before creating a note
+        template_handling = "smart",
+
+        -- path handling:
+        --   this applies to:
+        --     - new_note()
+        --     - new_templated_note()
+        --     - follow_link() to non-existing note
+        --
+        --   it does NOT apply to:
+        --     - goto_today()
+        --     - goto_thisweek()
+        --
+        --   Valid options:
+        --     - smart: put daily-looking notes in daily, weekly-looking ones in weekly,
+        --              all other ones in home, except for notes/with/subdirs/in/title.
+        --              (default)
+        --
+        --     - prefer_home: put all notes in home except for goto_today(), goto_thisweek()
+        --                    except for notes with subdirs/in/title.
+        --
+        --     - same_as_current: put all new notes in the dir of the current note if
+        --                        present or else in home
+        --                        except for notes/with/subdirs/in/title.
+        new_note_location = "smart",
+
+        -- should all links be updated when a file is renamed
+        rename_update_links = true,
+
+        vaults = {
+          vault2 = {
+            -- alternate configuration for vault2 here. Missing values are defaulted to
+            -- default values from telekasten.
+            -- e.g.
+            -- home = "/home/user/vaults/personal",
+          },
+        },
+
+        -- how to preview media files
+        -- "telescope-media-files" if you have telescope-media-files.nvim installed
+        -- "catimg-previewer" if you have catimg installed
+        media_previewer = "telescope-media-files",
+      })
+    end
+  }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.json", "*.jsonc" },
+  -- enable wrap mode for json files only
+  command = "setlocal wrap",
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    -- let treesitter use bash highlight for zsh files as well
+    require("nvim-treesitter.highlight").attach(0, "bash")
+  end,
+})
