93fb8c13dc9110b51efb644d3674fb3a2a16b316
Tyler Schwemley <me@tylerschwemley.com>
2022-09-23 19:14:12 +0000

    updated lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -10,57 +10,15 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.colorscheme = "gruvbox-material"
 lvim.builtin.lualine.options.theme = "gruvbox-material"
 
 vim.g.gruvbox_material_background = 'medium'
 vim.g.gruvbox_material_foreground = 'material'
--- lvim.builtin.bufferline.options.indicator_icon
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
-
--- Toggle key mappings
-
-
-
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
--- unmap a default keymapping
--- override a default keymapping
--- vim.keymap.del("n", "<C-Up>")
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -69,107 +27,69 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.dap.active = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
-lvim.builtin.lir.active = false
 lvim.builtin.nvimtree.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
--- if you don't want all the parsers change this to a table of the ones you want
+-- Treesitter
+lvim.builtin.treesitter.rainbow.enable = true
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
-  "c",
   "css",
   "go",
-  "java",
   "javascript",
   "json",
   "lua",
   "php",
-  "python",
-  "rust",
   "typescript",
-  "tsx",
   "yaml"
 }
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-lvim.builtin.treesitter.rainbow.enable = true
 
--- generic LSP settings
+-- Lsp
+lvim.lsp.installer.setup.ensure_installed = {
+  "bash",
+  "go",
+}
 
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumeko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
+-- Debugging
+lvim.builtin.dap.on_config_done = function()
+  local dap = require("dap")
 
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
+  dap.adapters.delve = {
+    type = 'server',
+    port = '${port}',
+    executable = {
+      command = 'dlv',
+      args = { 'dap', '-l', '127.0.0.1:${port}' },
+    }
+  }
 
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
-
--- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+  -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+  dap.configurations.go = {
+    {
+      type = "delve",
+      name = "Debug",
+      request = "launch",
+      program = "${file}"
+    },
+    {
+      type = "delve",
+      name = "Debug test", -- configuration for debugging test files
+      request = "launch",
+      mode = "test",
+      program = "${file}"
+    },
+    -- works with go.mod packages and sub packages
+    {
+      type = "delve",
+      name = "Debug test (go.mod)",
+      request = "launch",
+      mode = "test",
+      program = "./${relativeFileDirname}"
+    }
+  }
+end
 
 -- Additional Plugins
 lvim.plugins = {
@@ -183,6 +103,38 @@ lvim.plugins = {
   -- Themes
   { "sainnhe/everforest" },
   { "sainnhe/gruvbox-material" },
+
+  -- Treesitter
+  { "ray-x/lsp_signature.nvim" },
+  { "p00f/nvim-ts-rainbow" },
+  {
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+      }
+    end
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      --https://github.com/simrat39/symbols-outline.nvim#configuration
+      require("symbols-outline").setup()
+    end
+  },
 
   -- Utility
   {
@@ -213,155 +165,6 @@ lvim.plugins = {
     end,
     requires = "nvim-lua/plenary.nvim"
   },
-  {
-    "renerocksai/telekasten.nvim",
-    config = function()
-      local home = "/home/tschwemley/logseq"
-      require('telekasten').setup({
-        home = home,
-
-        -- if true, telekasten will be enabled when opening a note within the configured home
-        take_over_my_home = true,
-
-        -- auto-set telekasten filetype: if false, the telekasten filetype will not be used
-        --                               and thus the telekasten syntax will not be loaded either
-        auto_set_filetype = true,
-
-        -- dir names for special notes (absolute path or subdir name)
-        dailies = home .. '/' .. 'journals',
-        weeklies = nil,
-        templates = nil,
-        -- weeklies  = home .. '/' .. 'weekly',
-        -- templates = home .. '/' .. 'templates',
-
-        -- image (sub)dir for pasting
-        -- dir name (absolute path or subdir name)
-        -- or nil if pasted images shouldn't go into a special subdir
-        image_subdir = "img",
-
-        -- markdown file extension
-        extension = ".md",
-
-        -- Generate note filenames. One of:
-        -- "title" (default) - Use title if supplied, uuid otherwise
-        -- "uuid" - Use uuid
-        -- "uuid-title" - Prefix title by uuid
-        -- "title-uuid" - Suffix title with uuid
-        new_note_filename = "title",
-        -- file uuid type ("rand" or input for os.date()")
-        uuid_type = "%Y%m%d%H%M",
-        -- UUID separator
-        uuid_sep = "-",
-
-        -- following a link to a non-existing note will create it
-        follow_creates_nonexisting = true,
-        dailies_create_nonexisting = true,
-        weeklies_create_nonexisting = false,
-
-        -- skip telescope prompt for goto_today and goto_thisweek
-        journal_auto_open = false,
-
-        -- template for new notes (new_note, follow_link)
-        -- set to `nil` or do not specify if you do not want a template
-        -- template_new_note = home .. '/' .. 'templates/new_note.md',
-        template_new_note = nil,
-
-        -- template for newly created daily notes (goto_today)
-        -- set to `nil` or do not specify if you do not want a template
-        -- template_new_daily = home .. '/' .. 'templates/daily.md',
-        template_new_daily = nil,
-
-        -- template for newly created weekly notes (goto_thisweek)
-        -- set to `nil` or do not specify if you do not want a template
-        template_new_weekly = nil,
-
-        -- image link style
-        -- wiki:     ![[image name]]
-        -- markdown: ![](image_subdir/xxxxx.png)
-        image_link_style = "markdown",
-
-        -- default sort option: 'filename', 'modified'
-        sort = "filename",
-
-        -- integrate with calendar-vim
-        plug_into_calendar = false,
-        calendar_opts = {
-          -- calendar week display mode: 1 .. 'WK01', 2 .. 'WK 1', 3 .. 'KW01', 4 .. 'KW 1', 5 .. '1'
-          weeknm = 4,
-          -- use monday as first day of week: 1 .. true, 0 .. false
-          calendar_monday = 1,
-          -- calendar mark: where to put mark for marked days: 'left', 'right', 'left-fit'
-          calendar_mark = 'left-fit',
-        },
-
-        -- telescope actions behavior
-        close_after_yanking = false,
-        insert_after_inserting = true,
-
-        -- tag notation: '#tag', ':tag:', 'yaml-bare'
-        tag_notation = "#tag",
-
-        -- command palette theme: dropdown (window) or ivy (bottom panel)
-        command_palette_theme = "ivy",
-
-        -- tag list theme:
-        -- get_cursor: small tag list at cursor; ivy and dropdown like above
-        show_tags_theme = "ivy",
-
-        -- when linking to a note in subdir/, create a [[subdir/title]] link
-        -- instead of a [[title only]] link
-        subdirs_in_links = true,
-
-        -- template_handling
-        -- What to do when creating a new note via `new_note()` or `follow_link()`
-        -- to a non-existing note
-        -- - prefer_new_note: use `new_note` template
-        -- - smart: if day or week is detected in title, use daily / weekly templates (default)
-        -- - always_ask: always ask before creating a note
-        template_handling = "smart",
-
-        -- path handling:
-        --   this applies to:
-        --     - new_note()
-        --     - new_templated_note()
-        --     - follow_link() to non-existing note
-        --
-        --   it does NOT apply to:
-        --     - goto_today()
-        --     - goto_thisweek()
-        --
-        --   Valid options:
-        --     - smart: put daily-looking notes in daily, weekly-looking ones in weekly,
-        --              all other ones in home, except for notes/with/subdirs/in/title.
-        --              (default)
-        --
-        --     - prefer_home: put all notes in home except for goto_today(), goto_thisweek()
-        --                    except for notes with subdirs/in/title.
-        --
-        --     - same_as_current: put all new notes in the dir of the current note if
-        --                        present or else in home
-        --                        except for notes/with/subdirs/in/title.
-        new_note_location = "smart",
-
-        -- should all links be updated when a file is renamed
-        rename_update_links = true,
-
-        vaults = {
-          vault2 = {
-            -- alternate configuration for vault2 here. Missing values are defaulted to
-            -- default values from telekasten.
-            -- e.g.
-            -- home = "/home/user/vaults/personal",
-          },
-        },
-
-        -- how to preview media files
-        -- "telescope-media-files" if you have telescope-media-files.nvim installed
-        -- "catimg-previewer" if you have catimg installed
-        media_previewer = "telescope-media-files",
-      })
-    end
-  }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
