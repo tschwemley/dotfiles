ad3553afe1a32ee3e9b027af88dee531c7727aa8
Tyler Schwemley <me@tylerschwemley.com>
2022-12-18 17:22:42 -0500

    add nix home manager

--- .config/nvim/after/plugin/nvimtree.lua
+++ .config/nvim/after/plugin/nvimtree.lua
@@ -1,9 +1,7 @@
 require("nvim-tree").setup({
-  create_in_closed_folder = true,
   respect_buf_cwd = true,
   update_focused_file = {
         enable = true,
-        update_cwd = true,
-        ignore_list = {},
+        update_root = true,
   },
 })
